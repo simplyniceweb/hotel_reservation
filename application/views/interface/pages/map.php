62c3e7fa406bf9fcbc4bea5513b106a982377fb2
Jaylord Ferrer <simplyniceweb@gmail.com>
2014-07-08 23:11:46 +0800

    House rules and Map

--- application/views/interface/pages/map.php
+++ application/views/interface/pages/map.php
@@ -6,6 +6,105 @@ include(__DIR__.'/../nav.php');
 <div class="wrapper container round">
 	<div class="col-md-12 md-spacer"></div>
 	<h1 class="title">MAP & LOCATION</h1>
+	<p>Laiya Coco Grove's area is so vast that it can accomodate a huge number of guests. We can accommodate 200 overnight stay-in guests and as many as 8,000 day tour guests. Laiya is situated at the easternmost part of San Juan, Batangas. Laiya Aplaya is very well known for white sand beach resorts and is a famous tourist destination.</p>
+    <div class="row">
+        <div class="col-md-12" style="padding: 20px">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d15507.156488660437!2d121.401583!3d13.670583!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1404831120634" frameborder="0" style="border:0;width: 100%;height:600px;"></iframe>
+        </div>
+    	<div class="col-md-12" style="padding: 20px">
+        	<div class="col-md-6">
+            <h3 class="title">DIRECTIONS</h3>
+            <img src="<?=base_url()?>assets/images/map/direction.png" class="img-responsive">
+            </div>
+        	<div class="col-md-6">
+            <h3 class="title">TRANSPORTATIONS</h3>
+            	<button class="btn btn-info btn-lg btn-block" data-toggle="collapse" data-target="#private">Private Transport</button>
+                <div id="private" class="col-md-12 collapse">
+                <p></p>
+                <p>From Metro Manila via the South Luzon Expressway (SLEX) which ends in Calamba. There is a 7km stretch up to the Sto. Tomas entry to the Southern Tagalog Arterial Road (STAR Toll way) which now extends up to Batangas City.</p>
+                <ul>
+                <li> Option 1:
+                    <ol>
+                        <li>
+                        Continue along the STAR Tollway to the San Jose, Ibaan Exit 11kms. of road that narrows to a 2-lane highway just after the Lipa City Exit.
+                        
+                        </li>
+                        <li>
+                        Take the San Jose/ Ibaan exit turning left to Ibaan, following the main national road you will travel through Rosario up to San Juan.
+                        </li>
+                    </ol>
+                </li>
+                
+                <li> Option 2:
+                    <ol>
+                        <li>
+                        Leave the STAR Tollway at the Lipa City Exit 20km.
+                        </li>
+                        <li>
+                        Approaching Lipa City you will see in the distance a McDonald's at a "Y" junction, veer right to the main road. To the left of the road, you'll see a Jollibee sign. Just before the sign, a cross junction, turn right on P. Torres St., One (1)km or so on your right is a Shell Gas Station, a good place to stop as facilities are clean.
+                        </li>
+                    <li>
+                    Continue along the national road to Padre Burgos, upon reaching Padre Burgos you have two options:
+                        <ul>
+                            <li>
+                            Option 1 - go straight to Rosario clearly marked
+                            </li>
+                            <li>
+                            Option 2 - turn left a few hundred meters to the public market then turn right at the public market and animal auction facility. Auctions are held every Friday expect a half hour or more delay, particularly in the early morning up to about 3pm. If you don't mind the delay the hustle and bustle created by animals and vendors are interesting local color. The road from Padre Garcia rejoins the national road going to San Juan. If you decide to avoid the animal auction on Friday, proceed straight to Rosario.
+                            </li>
+                        </ul>
+                    </li>
+                        <li>
+                        The road ends at a stop light controlled heavy traffic junction. There is a Jollibee on the right. Turn left towards San Juan. This junction is where the road from Ibaan joins the national road to San Juan.
+                        </li>
+                    </ol>
+                </li>
+                </ul>
+                <p>Using option 1 is maybe the better choice as it avoids possible traffic in Lipa City and is a more pleasant journey overall.</p>
+                <p><i>Total travel time easy driving is under 3 hours from Makati</i></p>
+                </div>
+                <br/>
+            	<button class="btn btn-info btn-lg btn-block" data-toggle="collapse" data-target="#public">Public Transport</button>
+                <div id="public" class="col-md-12 collapse">
+                <p></p>
+                <ul>
+                <li>
+                From Makati Area
+                <ol>
+                    <li>
+                    Board a Southbound bus to Lipa City at the Taft Avenue LRT-Buendia Bus Station | Fare = P124.00(air-conditioned bus)
+                    </li>
+                    <li>
+                    At Lipa City JAM Bus Station walking distance to your left you will find Vans waiting for passengers to San Juan. Tell the driver to stop at the San Juan Public Market | Fare = P60.00
+                    </li>
+                    <li>
+                    At San Juan Public Market - Jeepneys are lined-up for Laiya. They all know Laiya Coco Grove and will drop you off at the resort main entrance | Fare = P35.00 (The driver will ask for an additional P5.00 drop you off right at the resort).
+                    </li>
+                </ol>
+                </li>
+                    <li>
+                    From Quezon City Area
+                    <ol>
+                        <li>
+                        Board a Southbound - To Lucena (JAM or JAC Bus) at Kamuning/Kamias Edsa Quezon City | Fare(approximately) = P200.00 (air-conditioned bus).
+                        </li>
+                        <li>
+                        At Candelaria-Quezon, tell the bus conductor to stop at Sambat.
+                        </li>
+                        <li>
+                        At Sambat, jeepneys are lined up to go to San Juan | Fare = P24.00.
+                        </li>
+                        <li>
+                        At San Juan Public Market terminal across Manolo's Mini Mart - Jeepneys are lined-up, they all know Laiya Coco Grove | Fare = P35.00
+                        </li>
+                    </ol>
+                    </li>
+                </ul>
+                <p><i>Total trip from Makati City : about 3-4 hours.</i></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php include(__DIR__.'/../footer.php'); ?>
